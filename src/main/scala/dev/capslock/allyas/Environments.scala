d2d6dfee72f36014745cc0c419665c521c345389
windymelt <windymelt@3qe.us>
2025-07-13 04:31:45 +0900

    wip

--- src/main/scala/dev/capslock/allyas/Environments.scala
+++ src/main/scala/dev/capslock/allyas/Environments.scala
@@ -3,4 +3,5 @@ package dev.capslock.allyas
 object Environments {
   val SHELL: String = sys.env.getOrElse("SHELL", "sh")
   val ALLYAS_CONF: Option[String] = sys.env.get("ALLYAS_CONF")
+  val ALLYAS_SHIM_DIR: Option[String] = sys.env.get("ALLYAS_SHIM_DIR")
 }
--- src/main/scala/dev/capslock/allyas/Main.scala
+++ src/main/scala/dev/capslock/allyas/Main.scala
@@ -11,27 +11,32 @@ import cats.implicits.*
 inline def eprintln(msg: String): Unit = Console.err.println(msg)
 
 case class Config(
-  configFile: Option[String],
-  verbose: Boolean = false
+    configFile: Option[String],
+    verbose: Boolean = false
 )
 
 object Main {
   val verboseFlag = Opts.flag("verbose", "Enable verbose output", "v").orFalse
-  
+
   val command = Command(
     name = "allyas",
     header = "A command aliasing tool"
   )(verboseFlag)
 
   def main(args: Array[String]): Unit = {
-    command.parse(args.toList) match {
-      case Left(help) =>
-        eprintln(help.toString)
-        sys.exit(1)
-      case Right(verbose) =>
-        val config = Config(Environments.ALLYAS_CONF, verbose)
-        val exitCode = run(config)
-        sys.exit(exitCode)
+    if (args.length > 0 && args(0) == "shim") {
+      val exitCode = runShim()
+      sys.exit(exitCode)
+    } else {
+      command.parse(args.toList) match {
+        case Left(help) =>
+          eprintln(help.toString)
+          sys.exit(1)
+        case Right(verbose) =>
+          val config = Config(Environments.ALLYAS_CONF, verbose)
+          val exitCode = run(config)
+          sys.exit(exitCode)
+      }
     }
   }
 
@@ -42,18 +47,19 @@ object Main {
         eprintln("*** Allyas: Please set the ALLYAS_CONF environment variable.")
         return 1
     }
-    
+
     if (config.verbose) {
       eprintln(s"*** Allyas: Using config file: $confFile")
     }
 
-    val confString = try {
-      os.read(os.Path(confFile, os.pwd))
-    } catch {
-      case e: Exception =>
-        eprintln(s"*** Allyas: Error reading config file: ${e.getMessage}")
-        return 1
-    }
+    val confString =
+      try {
+        os.read(os.Path(confFile, os.pwd))
+      } catch {
+        case e: Exception =>
+          eprintln(s"*** Allyas: Error reading config file: ${e.getMessage}")
+          return 1
+      }
 
     val parser = SExpressionParser()
     val conf = parser.parse(parser.config, confString)
@@ -63,7 +69,17 @@ object Main {
       return 1
     }
 
-    val whoami = os.RelPath(filename).baseName
+    val whoami = if (filename.startsWith("/")) {
+      os.Path(filename).baseName
+    } else {
+      os.RelPath(filename).baseName
+    }
+
+    if (whoami == "allyas") {
+      println(command.showHelp)
+      return 0
+    }
+
     conf.get.get(whoami) match {
       case Some(cmd) =>
         if (config.verbose) {
@@ -86,5 +102,69 @@ object Main {
         127
     }
   }
+
+  def runShim(): Int = {
+    if (Environments.ALLYAS_CONF.isEmpty) {
+      eprintln("*** Allyas: Please set the ALLYAS_CONF environment variable.")
+      return 1
+    }
+
+    if (Environments.ALLYAS_SHIM_DIR.isEmpty) {
+      eprintln(
+        "*** Allyas: Please set the ALLYAS_SHIM_DIR environment variable."
+      )
+      return 1
+    }
+
+    val confFile = Environments.ALLYAS_CONF.get
+    val shimDir = Environments.ALLYAS_SHIM_DIR.get
+
+    val confString =
+      try {
+        os.read(os.Path(confFile, os.pwd))
+      } catch {
+        case e: Exception =>
+          eprintln(s"*** Allyas: Error reading config file: ${e.getMessage}")
+          return 1
+      }
+
+    val parser = SExpressionParser()
+    val conf = parser.parse(parser.config, confString)
+
+    if (!conf.successful) {
+      eprintln(s"*** Allyas: Error parsing config file: ${conf.toString}")
+      return 1
+    }
+
+    val executablePath = if (filename.startsWith("/")) {
+      os.Path(filename)
+    } else {
+      os.pwd / os.RelPath(filename)
+    }
+    val shimDirPath = os.Path(shimDir)
+
+    try {
+      if (!os.exists(shimDirPath)) {
+        os.makeDir.all(shimDirPath)
+        println(s"Created shim directory: $shimDir")
+      }
+
+      val createdLinks = conf.get.keys.map { aliasName =>
+        val linkPath = shimDirPath / aliasName
+        if (os.exists(linkPath)) {
+          os.remove(linkPath)
+        }
+        os.symlink(linkPath, executablePath)
+        aliasName
+      }.toList
+
+      println(s"Created ${createdLinks.size} symlinks in $shimDir:")
+      createdLinks.foreach(name => println(s"  $name"))
+      0
+    } catch {
+      case e: Exception =>
+        eprintln(s"*** Allyas: Error creating symlinks: ${e.getMessage}")
+        1
+    }
+  }
 }
-// TODO: autolinking
